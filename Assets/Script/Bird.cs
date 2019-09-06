baf06712f7a6b2839807b107cd5ca019f3ff186d
IrvingOlvera <irving_alan18@hotmail.com>
2019-09-06 15:25:45 -0500

    juego FlappyBird con unity

---
+++ Assets/Script/Bird.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    public float upForce = 200f;
+    private Animator anim;
+
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("flap");
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+        anim.SetTrigger("die");
+        GameController.instance.BirdDie();
+        rb2d.velocity = Vector2.zero;
+    }
+}
---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    public GameObject gameOverText;
+    public bool gameOver;
+    public float scrollSpeed = -1.5f;
+
+    private int score= 0;
+    public Text scoreText;
+
+    private void Awake()
+    {
+        if(GameController.instance == null)
+        {
+            GameController.instance = this;
+        }else if (GameController.instance != this)
+        {
+            Destroy(gameObject);
+            Debug.LogWarning("Gamecontroller ha sido instaciado por segunda vez");
+        }
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void BirdDie()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+    private void OnDestroy()
+    {
+        if (GameController.instance == this)
+        {
+            GameController.instance = null;
+        }
+    }
+
+    public void BirdScore()
+    {
+        if (gameOver) return;
+
+        score++;
+        scoreText.text = "Score" + score;
+    }
+}
